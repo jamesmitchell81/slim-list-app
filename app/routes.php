5c584edc3fb8ed7092a48dc229539d76d8f1e58c
James Mitchell <james.mitchell81@live.co.uk>
2017-04-11 17:12:08 +0100

    added controller code to /lists route

--- app/routes.php
+++ app/routes.php
@@ -10,4 +10,20 @@
 
 
 $app->get('/', 'App\Controllers\HomeController:home');
-$app->get('/lists', 'App\Controllers\ListController:display');
+// $app->get('/lists', 'App\Controllers\ListController:display');
+
+$app->get('/lists', function($request, $response, $args) {
+	$db = $this->database->connection();
+
+	$user_id = $_SESSION['user_id'];
+	$SQL = "SELECT * FROM app_lists WHERE user_id = :user_id";
+
+	$statement = $db->prepare($SQL);
+	$statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+	$statement->execute();
+	$lists = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+	$args['lists'] = $lists;
+
+	return $this->renderer->render($response, 'lists.phtml', $args);
+});
